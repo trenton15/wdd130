0cd8acc20c38271768886c3843e823aadd526141
trenton15 <y4trenton@gmail.com>
2022-09-23 23:50:08 -0600

    java

--- about_me/dynamic.js
+++ about_me/dynamic.js
@@ -1,7 +1,26 @@
-const PI = 3.14;
-const radius = 3;
-let area = 0;
-console.log(radius)
-area = radius * radius * PI;
-radius = 4;
-area = radius * radius * PI;
+// Favorite Foods
+const favoriteFoods = ["Seafood", " Cookies", " Ice-cream"];
+const foodList = document.createElement('p');
+foodList.textContent = `Favorite Foods: ${favoriteFoods}`;
+document.body.appendChild(foodList);
+favoriteFoods.shift();
+foodList.textContent = `Favorite Foods2:  ${favoriteFoods}`;
+favoriteFoods.push(" Sandwiches");
+foodList.textContent = `Favorite Foods3:  ${favoriteFoods}`;
+
+// Todays Date
+const weekday = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday"
+];
+const today = new Date();
+console.log(today);
+const day = today.getDay()
+console.log(day);
+const theDay = document.querySelector('#displayToday');
+theDay.textContent = weekday[day];

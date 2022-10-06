ac3375e1cd2a85b1e8df041dd5aed11390913ca1
trenton15 <y4trenton@gmail.com>
2022-10-06 08:47:31 -0600

    new code

--- sandbox/debugging.js
+++ sandbox/debugging.js
@@ -1,21 +1,58 @@
-const PI = 3.14;
-const radius = 3;
-let area = 0;
-area = radius * radius * PI;
-radius = 4;
-area = radius * radius * PI;
+// events.js
+let tasks = [];
 
-// get age from input
-const age = document.getElementById("age").value; // assume 21 was entered
-// add 10 years
-const agePlus10 = parseInt(age) + 10;
-console.log(agePlus10);
+function renderTasks(tasks) {
+  // get the list element from the DOM
+  const listElement = document.querySelector('#todoList');
+  // make sure it is empty
+  listElement.innerHTML = '';
+  // loop through the tasks array. for each of them we need to add the HTML markup for a todo.
+  console.log(tasks);
+}
 
-// modify the copyInput callback to receive the event object
-function copyInput(event) {
-    // take a look at the event!
-    console.log(event);
-    const inputElement = document.getElementById("inputBox");
-    const outputElement = document.getElementById("output");
-    outputElement.innerHTML = inputElement.value;
-  }
+function newTask() {
+  // get the value entered into the #todo input
+  const todoElement = document.querySelector('#todo');
+  const value = todoElement.value;
+  // add it to our arrays tasks
+  tasks.push({ detail: value, completed: false});
+  // render out the list.
+  renderTasks(tasks);
+}
+
+function removeTask(taskElement) {
+  // note the use of Array.filter to remove the element from our task array
+  tasks = tasks.filter(
+    (task) => task.detail != taskElement.childNodes[0].innerText
+  );
+  // this line removes the HTML element from the DOM
+  taskElement.remove();
+}
+
+function completeTask(taskElement) {
+  // In this case we need to find the index of the task so we can modify it.
+  const taskIndex = tasks.findIndex(
+    (task) => task.detail === taskElement.childNodes[0].innerText
+  );
+  // once we have the index we can modify the complete field.
+  // tasks[taskIndex].completed ? false : true is a ternary expression.
+  // If the first part is true (left of the ?), then the value on the left of the : will get returned, otherwise the value on the right of the : will be returned.
+  tasks[taskIndex].completed = tasks[taskIndex].completed ? false : true;
+  // toggle adds a class if it is not there, removes it if it is.
+  taskElement.classList.toggle("strike");
+  console.log(tasks);
+}
+
+function manageTasks(event) {
+  // did they click the delete or complete icon?
+  console.log(event.target);
+  console.log(event.currentTarget);
+  // event.target will point to the actual icon clicked on. We need to get the parent li to work with however. HINT: Remember element.closest()? Look it up if you don't
+
+  // because we added 'data-function="delete"' to each icon in a task we can access a dataset property on our target
+  // use that in a couple of if statements to decide whether to run removeTask or completeTask
+}
+
+// we need to attach listeners to the submit button and the list. Listen for a click, call the 'newTask' function on submit and call the 'manageTasks' function if either of the icons are clicked in the list of tasks.
+const buttonElement = document.getElementById("#submitTask");
+buttonElement.addEventListener("click", newTask);

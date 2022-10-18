ff78eeb9dfc9b4c35c21ccd6af865ff29e04d4da
trenton15 <y4trenton@gmail.com>
2022-10-18 08:25:58 -0600

    array and new path

---
+++ sandbox/arrays.js
@@ -0,0 +1,19 @@
+// example 1
+const steps = ["one", "two", "three"];
+// callback declaration
+function makeList(item) {
+  const listElement = document.getElementById("myList");
+  listElement.innerHTML += `<li>${item}</li>`;
+}
+steps.forEach(makeList);
+
+// example 2
+// is the luckyNumber in the list?
+const myArray = [12, 34, 21, 54];
+const luckyNumber = 21;
+let luckyIndex = -1;
+myArray.forEach(function (item, index) {
+  if (item === luckyNumber) {
+    luckyIndex = index;
+  }
+});

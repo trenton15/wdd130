e509659218d5242cde63552ad232e6da2206ec5f
trenton15 <y4trenton@gmail.com>
2022-10-21 09:31:11 -0600

    java complete

--- blog/articles.js
+++ blog/articles.js
@@ -9,7 +9,8 @@ const articles = [
       imgAlt: "Book cover for Septimus Heap 1",
       ages: "10-14",
       genre: "Fantasy",
-      stars: "⭐⭐⭐"
+      stars: "⭐⭐⭐",
+      link: "https://en.wikipedia.org/wiki/Magyk"
     },
     {
       id: 2,
@@ -22,7 +23,22 @@ const articles = [
       imgAlt: "Book cover for Magnus Chase 1",
       ages: "12-16",
       genre: "Fantasy",
-      stars: "⭐⭐⭐⭐"
+      stars: "⭐⭐⭐⭐",
+      link: "https://en.wikipedia.org/wiki/The_Sword_of_Summer"
+    },
+    {
+      id: 3,
+      title: "Belgariad Book One: Pawn of Prophecy",
+      date: "Feb 12, 2022",
+      description:
+        'A fierce dispute among the Gods and the theft of a powerful Orb leaves the World divided into five kingdoms. Young Garion, with his "Aunt Pol" and an elderly man calling himself Wolf --a father and daughter granted near-immortality by one of the Gods -- set out on a complex mission.',
+      imgSrc:
+        "https://images-na.ssl-images-amazon.com/images/I/41ZxXA+nInL.jpg",
+      imgAlt: "Book cover for Pawn of Prophecy",
+      ages: "12-16",
+      genre: "Fantasy",
+      stars: "⭐⭐⭐⭐⭐",
+      link: "https://www.goodreads.com/book/show/44659.Pawn_of_Prophecy"
     }
   ];
 
@@ -36,10 +52,10 @@ const articles = [
             <p>${article.stars}</p>
         </div>
         <div>
-            <h2><a href="#">${article.title}</a></h2>
-            <img alt="${article.imgAlt}" src=${article.imgSrc}>
+            <h2><a href="${article.link}" target="_blank">${article.title}</a></h2>
+            <a href="${article.link}" target="_blank"><img alt="${article.imgAlt}" src=${article.imgSrc}></a>
             <p>${article.description}
-                <a class="link" href="#">Read More...</a>
+                <a class="link" href="${article.link}" target="_blank">Read More...</a>
             </p>
         </div>
     </article>

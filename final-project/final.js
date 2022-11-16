54569834dfbccee7d9ad7cf6e9c37571e6169771
trenton15 <y4trenton@gmail.com>
2022-11-15 19:05:34 -0700

    design

---
+++ final-project/final.js
@@ -0,0 +1,48 @@
+// Render the navigation bar
+function navTemplate() {
+    return `
+    <div class="nav" id="topNav">
+        <a href="/wdd130/final-project/final.html">Home</a>
+        <a href="/wdd130/final-project/schedule.html">Schedule</a>
+        <a href="/wdd130/final-project/contact.html">Contact</a>
+        <a href="javascript:void(0);" class="icon" onclick="addResponsive()">
+            <i class="fa fa-bars"></i>
+        </a>
+    </div>`
+}
+
+function renderNav() {
+    // Get element
+    const element = document.querySelector('nav');
+    // Build html string
+    const html = navTemplate()
+    // Insert html into element
+    element.innerHTML = html;
+}
+
+/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
+function addResponsive() {
+    var x = document.getElementById("topNav");
+    if (x.className === "nav") {
+      x.className += " responsive";
+    } else {
+      x.className = "nav";
+    }
+}
+
+// Render the footer
+function footerTemplate() {
+    return `<p>&copy2022 Trenton Murdock</p>`
+}
+
+function renderFooter() {
+    // Get element
+    const element = document.querySelector('footer');
+    // Build html string
+    const html = footerTemplate()
+    // Insert html into element
+    element.innerHTML = html;
+}
+
+renderNav();
+renderFooter();

e098f8ec51ee25d7c981722bcb00156ce1e9538a
Trenton Murdock <81528125+trenton15@users.noreply.github.com>
2024-01-13 14:48:07 -0600

    save

--- final-project/final.js
+++ final-project/final.js
@@ -61,12 +61,12 @@ function addResponsive() {
 function footerTemplate() {
     return `
     <img class="logo-icon" src="/wdd130/images/salon_logo_icon2.png" alt="icon logo">
-    <P class="address"><img src="/wdd130/images/pin_icon.png"> 259 E Main St, Rexburg, ID</P>
-    <p class="email"><img src="/wdd130/images/email_icon.png"> jana@gmail.com</p>
-    <p class="phone"><img src="/wdd130/images/phone_icon.png"> (555) 555-555</p>
+    <P class="address"><img src="/wdd130/images/pin_icon.png"> 1520 Sandpiper St, Centerton, AR 72719</P>
+    <p class="email"><img src="/wdd130/images/email_icon.png"> jcmurdock369@gmail.com</p>
+    <p class="phone"><img src="/wdd130/images/phone_icon.png"> (602) 903-9454</p>
     <a href="https://www.instagram.com/hair.by.murdock/" class="insta-icon"> <img src="/wdd130/images/instagram_icon.png"></a>
     <a href="https://www.facebook.com/jana.hardy.77" class="face-icon"> <img src="/wdd130/images/facebook_icon.png"></a>
-    <p class="copy-right">&copy2022 Trenton Murdock</p>`
+    <p class="copy-right">&copy2024 Trenton Murdock</p>`
 }
 
 function renderFooter() {
